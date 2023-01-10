c9f337c155a00752efd7e7cc954e8a098952ad9f
Riffyraff <82163631+ArifAli999@users.noreply.github.com>
2023-01-10 02:07:52 +0000

    tst

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -16,6 +16,7 @@ function Dashboard() {
         try {
             const response = await fetch('https://randomuser.me/api');
             const data = await response.json();
+            console.log(data)
             return data.results;
 
         }
